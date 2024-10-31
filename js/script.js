d86b26ec7f7f482c857c188d2abd5986ea65624d
Gustavo Miranda <gugamiranda617@gmail.com>
2024-10-31 18:35:39 -0300

    Implementando Particulas

--- js/script.js
+++ js/script.js
@@ -41,3 +41,136 @@ window.addEventListener('load', animateOnScroll)
 
 // Inicia o efeito de digitação ao carregar a página
 window.onload = iniEfDig;
+
+
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particlesArrey;
+
+// Pega a posição do mouse
+let mouse = {
+    x: null,
+    y: null,
+    radius: (canvas.height/80) * (canvas.width/80)
+}
+
+window.addEventListener('mousemove',
+    function(event) {
+        mouse.x = event.x;
+        mouse.y = event.y;
+    }
+);
+
+//Criando a particula
+class Particle {
+    constructor(x, y, directionX, directionY, size, color) {
+        this.x = x;
+        this.y = y;
+        this.directionX = directionX;
+        this.directionY = directionY;
+        this.size = size;
+        this.color = color;
+    }
+    //metodo para fazer individuais
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
+        ctx.fillStyle = '#8C5523';
+        ctx.fill();
+    }
+    //checa a posição da particula e do mouse
+    update() {
+        if (this.x > canvas.width || this.x < 0) {
+            this.directionX = -this.directionX;
+        }
+        if (this.y > canvas.height || this.y < 0) {
+            this.directionY = -this.directionY;
+        }
+
+        let dx = mouse.x - this.x;
+        let dy = mouse.y - this.y;
+        let distance = Math.sqrt(dx*dx + dy*dy);
+        if (distance < mouse.radius + this.size){
+            if (mouse.x < this.x && this.x < canvas.width - this.size * 10) {
+                this.x += 10;
+            }
+            if (mouse.x > this.x && this.x > this.size * 10) {
+                this.x -= 10;
+            }
+            if (mouse.y > this.y && this.y < canvas.height - this.size * 10) {
+                this.y += 10;
+            }
+            if (mouse.y > this.y && this.y > this.size * 10) {
+                this.y -= 10;
+            }
+        }
+        this.x += this.directionX;
+        this.y += this.directionY;
+        this.draw();
+    }
+}
+
+function init() {
+    particlesArrey = [];
+    let numberOfParticles = (canvas.height * canvas.width) / 9000;
+    for (let i = 0; i < numberOfParticles*2; i++) {
+        let size = (Math.random() * 5) + 1;
+        let x = (Math.random() * ((innerWidth - size * 2) - (size * 2)) + size *2);
+        let y = (Math.random() * ((innerHeight - size * 2) - (size * 2)) + size *2);
+        let directionX = (Math.random() * 5) - 2.5;
+        let directionY = (Math.random() * 5) - 2.5;
+        let color = '#8C5523';
+
+        particlesArrey.push(new Particle(x, y, directionX, directionY, size, color));
+    }
+}
+
+function connect() {
+    let opacityValue = 1;
+    for (let a = 0; a < particlesArrey.length; a++) {
+        for (let b = a; b < particlesArrey.length; b++) {
+            let distance = (( particlesArrey[a].x - particlesArrey[b].x) * (particlesArrey[a].x - particlesArrey[b].x)) + ((particlesArrey[a].y - particlesArrey[b].y) * (particlesArrey[a].y - particlesArrey[b].y));
+            if (distance < (canvas.width/7) * (canvas.height/7)) {
+                opacityValue = 1 - (distance/20000);
+                ctx.strokeStyle = 'rgba(140,85,31,' + opacityValue + ')';
+                ctx.lineWidth = 1;
+                ctx.beginPath();
+                ctx.moveTo(particlesArrey[a].x, particlesArrey[a].y);
+                ctx.lineTo(particlesArrey[b].x, particlesArrey[b].y);
+                ctx.stroke();
+            }
+        }
+    }
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, innerWidth, innerHeight);
+    for (let i = 0; i < particlesArrey.length; i++) {
+        particlesArrey[i].update();
+    }
+    connect();
+}
+
+window.addEventListener('resize',
+    function(){
+        canvas.width = innerWidth;
+        canvas.height = innerHeight;
+        mouse.radius = ((canvas,height/80) * (canvas.height/80));
+        init();
+    }
+);
+
+window.addEventListener('mouseout',
+    function(){
+        mouse.x = undefined;
+        mouse.y = undefined;
+    }
+);
+
+
+init();
+animate();
